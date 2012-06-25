5a4f51acb5c517fb0f500186c63c16ae0c49c666
Pulkit Singhal <pulkitsinghal@gmail.com>
2012-06-25 10:42:30 -0500

    Initial Commit

---
+++ ESClient/ESClient.h
@@ -0,0 +1,13 @@
+//
+//  ESClient.h
+//  ESClient
+//
+//  Created by Pulkit Singhal on 6/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ESClient : NSObject
+
+@end
---
+++ ESClient/ESClient.m
@@ -0,0 +1,13 @@
+//
+//  ESClient.m
+//  ESClient
+//
+//  Created by Pulkit Singhal on 6/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ESClient.h"
+
+@implementation ESClient
+
+@end
